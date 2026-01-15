512a536fa82d5d88570f986ad4e7d9abccef1128
mendelsshop <mendelsshop@gmail.com>
2026-01-14 19:37:49 -0500

    switching to normal sexpr recursive descent parser

--- lib/parser.ml
+++ lib/parser.ml
@@ -10,20 +10,24 @@ type state = {
 
 let init_state = { braces = []; current = ""; result = Error Empty }
 
-let parse_sexpr (string : string) : (sexpr, error) result =
-  (* maybe just go back to recursive descent *)
-  (String.fold_left
-     (fun state char ->
-       match state.result with
-       | Ok _ -> (
-           match char with
-           | '{' | '[' | '(' ->
-               let state = { state with braces = char :: state.braces } in
-               failwith ""
-           | '}' | ']' | ')' -> failwith ""
-           | ' ' | '\t' | '\n' -> state
-           | _ -> failwith "")
-       | Error Empty -> failwith ""
-       | Error _ -> state)
-     init_state string)
-    .result
+let split p l =
+  let rec aux f = function
+    | x :: l when p x -> aux (fun xs -> f (x :: xs)) l
+    | rest -> (f [], rest)
+  in
+  aux Fun.id l
+
+let rec parse_sexpr (string : char list) =
+  match string with
+  | ('[' | '{' | '(') :: string -> failwith ""
+  | (']' | '}' | ')') :: string -> failwith ""
+  | ('\t' | ' ' | '\n') :: string -> parse_sexpr string
+  | char :: string ->
+      let symbol, string =
+        split
+          (fun x ->
+            List.mem x [ '['; '{'; '('; ']'; '}'; ')'; '\t'; ' '; '\n' ])
+          string
+      in
+      (`Normal (Symbol (char :: symbol |> List.to_seq |> String.of_seq)), string)
+  | [] -> (`empty, [])
